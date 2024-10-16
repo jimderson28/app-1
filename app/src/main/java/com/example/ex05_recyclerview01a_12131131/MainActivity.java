1adbd85ae86f7a9848646f658d804f3731137e5f
jimderson28 <jim.chu28@gmail.com>
2024-10-16 14:50:45 +0800

    app-1016-10-2

--- app/src/main/java/com/example/ex05_recyclerview01a_12131131/MainActivity.java
+++ app/src/main/java/com/example/ex05_recyclerview01a_12131131/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex05_recyclerview01a_12131131;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +13,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 ArrayList<MyListData> mList = null;
 MyListAdapter adeapter = null;
     @Override
@@ -32,5 +35,42 @@ MyListAdapter adeapter = null;
         mList = MyListData.getMyListDataArrayList();
         adeapter =  new MyListAdapter(mList);
         recyclerView.setAdapter(adeapter);
+        Button btnInsert = findViewById(R.id.btnIster);
+        Button btndelete = findViewById(R.id.btnDelete);
+        btnInsert.setOnClickListener(this);
+        btndelete.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnIster:
+                InsertItem(v);
+            break;
+            case R.id.btnDelete:
+                DeleteItem(v);
+                break;
+        }
+
+    }
+
+    private void InsertItem(View v) {
+        int min = 0;
+        int max = 8;
+        Random random = new Random();
+        int index = random.nextInt((max - min)+1)+min;
+        MyListData newItem = new MyListData("new Item"+index, android.R
+                .drawable.ic_dialog_map);
+        mList.add(index, newItem);
+        adeapter.notifyItemInserted(index);
+    }private void DeleteItem(View v){
+        int min = 0;
+        int max = 8;
+        Random random = new Random();
+        int index = random.nextInt((max - min)+1)+min;
+        mList.remove(index);
+        adeapter.notifyItemRemoved(index);
+
     }
 }
